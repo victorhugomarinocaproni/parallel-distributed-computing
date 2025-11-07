7b647ad73e948bb44fe5c9b8ce781c8aa8732f68
Victor Caproni <victorcaproni04@gmail.com>
2025-11-07 16:45:12 -0300

    Update Distribuidor.java

--- cliente-program/Distribuidor/src/Distribuidor.java
+++ cliente-program/Distribuidor/src/Distribuidor.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Distribuidor {
-    static void main(String[] args) {
+    public static void main(String[] args) {
 
         System.out.println("========================================");
         System.out.println("APLICAÇÃO CLIENTE");
